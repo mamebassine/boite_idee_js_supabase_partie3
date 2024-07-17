0174bf9ddf7bc44c3046137c7aeb0b402a5d0ec5
mamebassine <mamebassine06@gmail.com>
2024-07-17 23:47:16 +0000

    Gestionnaire d'evenements

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 const supabaseUrl = 'https://tyjbimuyxvmenhaadfwq.supabase.co'; // URL de Supabase
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5amJpbXV5eHZtZW5oYWFkZndxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjEwNjY2NTIsImV4cCI6MjAzNjY0MjY1Mn0.qGJHPUHnDRRtcZEMAMysX2m81mtXsqGBQSJ3VcP5jag'; // Clé API de Supabase
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey); // Création du client Supabase
+
+// Ajouter un gestionnaire d'événements pour la soumission du formulaire
+document.getElementById('ideaForm').addEventListener('submit', async function(event) {
+    event.preventDefault(); // Empêche la soumission du formulaire
+
+    var libelle = document.getElementById('libelle').value; // Récupérer la valeur du champ "libelle"
+    var categorie = document.getElementById('categorie').value; // Récupérer la valeur du champ "categorie"
+    var message = document.getElementById('message').value; // Récupérer la valeur du champ "message"
